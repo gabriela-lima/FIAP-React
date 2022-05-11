bc1eb8759aaad0584db0f9ca99c055c30a047c04
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-05-11 15:09:48 -0300

    Fazendo ficar dinamico com um bd json

--- src/Tabela.js
+++ src/Tabela.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+
 
 function Tabela(){
 
@@ -7,35 +8,49 @@ function Tabela(){
 
 
   useEffect(() => {
-        axios.get('https://iot.14mob.com/lista.json').then( response => {
-            setUsuarios(response.data);
+        axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
+            setUsuarios(response.data.users);
             console.log(response);
         })
     },[])
 
 
   return (
-    <table className='minhaTabela'>
-        <thead>
-            <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            </tr>
-        
-        </thead>  
-        <tbody> 
-            { usuarios.map( usuario => {
-                return (
-                    <tr key={usuario.id}>
-                        <td>{usuario.id}</td>
-                        <td>{usuario.nome}</td>
-                        <td>{usuario.descricao}</td>
-                    </tr>
-                    )
-            } ) }
-        </tbody>
-      </table>
+      <div>
+        <form className="formulario">
+        <label>Nome</label>
+        <input name="name" value="" />
+        <label>Email</label>
+        <input name="email" value="" />
+        <label>senha</label>
+        <input name="password" value="" />
+
+        <button type="submit">Enviar</button>
+            
+        </form>
+        <table className='minhaTabela'>
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                </tr>
+            
+            </thead>  
+            <tbody> 
+                { usuarios.map( usuario => {
+                    return (
+                        <tr key={usuario.id}>
+                            <td>{usuario.id}</td>
+                            <td>{usuario.name}</td>
+                            <td>{usuario.email}</td>
+                        </tr>
+                        )
+                } ) }
+            </tbody>
+        </table>
+    </div>
+      
       
   );
 }
