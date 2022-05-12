2896b59f164fc0e2a859be25e3caa29ac92b3be8
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-05-12 11:26:00 -0300

    Criando chamada http com o metodo post

--- src/Tabela.js
+++ src/Tabela.js
@@ -4,8 +4,27 @@ import { useEffect, useState } from "react";
 
 function Tabela(){
 
-    const [usuarios, setUsuarios] = useState([]);
+    const [usuarios, setUsuarios] = useState([])
 
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+
+    function salvaFormulario(){
+        let parametros = {
+            name: nome,
+            email: email,
+            password: senha
+
+        }
+        axios.post('https://iot.14mob.com/api-fiap/public/index.php/users', parametros).then(response => {
+            if(response.status == 201){
+                alert('eba, deu certo')
+            }else{
+                alert('lascou')
+            }
+        }).catch(error => console.log(error))
+    }
 
   useEffect(() => {
         axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
@@ -17,17 +36,27 @@ function Tabela(){
 
   return (
       <div>
-        <form className="formulario">
+        <form className="formulario" onSubmit={event => {
+            event.preventDefault()
+            salvaFormulario()
+            return false
+        }}>
         <label>Nome</label>
-        <input name="name" value="" />
+        <input name="name" onChange={e => setNome(e.target.value)}/>
         <label>Email</label>
-        <input name="email" value="" />
+        <input name="email" onChange={e => setEmail(e.target.value)} />
         <label>senha</label>
-        <input name="password" value="" />
+        <input name="password" onChange={e => setSenha(e.target.value)}/>
 
         <button type="submit">Enviar</button>
             
         </form>
+
+        <p>{nome}</p>
+        <p>{email}</p>
+        <p>{senha}</p>
+
+
         <table className='minhaTabela'>
             <thead>
                 <tr>
