5904b97a5a17fddfab1a9da2219bd39a895dcfde
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-05-13 15:23:59 -0300

    Adicionando cartao

--- src/Cartao.js
+++ src/Cartao.js
@@ -1,6 +1,6 @@
 function Cartao(props){
     return(
-        <div className="card my-3">
+        <div className="card my-3 text-dark bg-light mb-3">
             <div className="card-body">
                     <h3 className="card-title titulos">{props.titulo}</h3>
                     {props.children}
