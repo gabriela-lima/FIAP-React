b030876653dd510f53ef55f7a730864d4d5b35e2
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-05-13 11:32:03 -0300

    Novo componente

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Lista from './Lista.js';
 
 
 
@@ -26,6 +27,7 @@ function App() {
       axios.post('https://iot.14mob.com/api-fiap/public/index.php/users', parametros).then(response => {
           if(response.status == 201){
               alert('Ebaaaaa deu certo')
+              atualizarLista()
           }else{
               alert('lascou')
           }
@@ -33,17 +35,7 @@ function App() {
 
   }
 
-  function removerUsuario(id){
-      console.log('funcionando' + id);
-
-      axios.delete("https://iot.14mob.com/api-fiap/public/index.php/users/" + id).then( response => {
-          alert('Deu certo removi o usuario')
-
-          window.location = "";
-
-      }).catch( error => console.log(error));
-
-  }
+  
 
   function atualizarUsuarioApi(){
 
@@ -55,6 +47,7 @@ function App() {
       axios.put('https://iot.14mob.com/api-fiap/public/index.php/users/'+ id, parametros).then(response => {
           if(response.status == 200){
               alert('Ebaaaaa deu certo')
+              atualizarLista()
           }else{
               alert('lascou')
           }
@@ -67,12 +60,15 @@ function App() {
       setEmail(usuario.email);
       setSenha(usuario.password);
   }
+
+  function atualizarLista(){
+    axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
+        setUsuarios(response.data.users);
+    })
+  }
   
 useEffect(() => {
-      axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
-          setUsuarios(response.data.users);
-          console.log(response);
-      })
+      atualizarLista();
   },[])
 
 
@@ -83,7 +79,7 @@ return (
           if(id != ''){
               atualizarUsuarioApi()
           }else{
-              salvarFormulario();
+              salvarFormulario()
           }
           return false;
       } } > 
@@ -101,33 +97,8 @@ return (
       <p>{ email }</p>
       <p>{ senha }</p>
       
-      <table className='minhaTabela'>
-          <thead>
-              <tr>
-              <th>ID</th>
-              <th>Nome</th>
-              <th>Email</th>
-              <th>Ações</th>
-              </tr>
-          
-          </thead>  
-          <tbody> 
-              { usuarios.map( usuario => {
-                  return (
-                      <tr key={usuario.id}>
-                          <td>{usuario.id}</td>
-                          <td>{usuario.name}</td>
-                          <td>{usuario.email}</td>
-                          <td>
-                              <button onClick={ event => atualizarUsuario(usuario) } > Editar </button>
-                              <button onClick={ event => removerUsuario(usuario.id) } > Deletar </button>
-                          </td>
-                          
-                      </tr>
-                      )
-              } ) }
-          </tbody>
-      </table>
+      <Lista usuarios={usuarios} atualizarLista = {e => atualizarLista()} onEditar= { usuario => atualizarUsuario(usuario) } ></Lista>
+
   </div>
     
     
